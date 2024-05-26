6c18d24f86d2c0146096705710054c653e5adf5c
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-05-25 19:13:45 -0700

    Set-up project with vite

---
+++ src/router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "./pages/HomePage";
+
+export function Router() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<Layout/>}>
+        <Route path="/" element={<HomePage />}/>
+      </Route>
+    )
+  )
+  return router;
+}
