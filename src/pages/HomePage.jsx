d39a522bd7a7db80ffc97a544cef79fac5f037d8
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-06 21:24:24 -0700

    Add navbar to landing page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,25 @@
 export default function HomePage() {
   return(
-    <div className="text-8xl">
-      Hello World
+    <div className="text-8sm">
+      <div className="home-main-container">
+        <div className="header">
+          <div className="logo">
+
+          </div>
+          <div className="nav-bar">
+            <nav>
+              <ul className="flex flex-row gap-10">
+                <li>About</li>
+                <li>Sign Up</li>
+                <li>Log In</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="main">
+
+        </div>
+      </div>
     </div>
   )
 }
