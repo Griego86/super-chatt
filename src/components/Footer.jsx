2ebe120b7e11c9cbe92f99b86b9daa8ad44e4dfd
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-07 13:19:48 -0700

    Fix footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
 
   return (
       <footer className="text-center py-5">
-          <p>Copyright&copy;2024 Capytech. All rights reserved.</p>
+          <p>Copyright&copy;2024. All rights reserved.</p>
       </footer>
   )
 }
