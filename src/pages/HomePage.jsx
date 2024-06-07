14a3a96c912dcc9de7eebd46cf710c62d566db81
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-07 13:21:46 -0700

    Remove default vite CSS styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,11 @@
 export default function HomePage() {
   return(
-    <div className="text-8sm">
-      <div className="home-main-container">
-        <div className="header">
-          <div className="logo">
-
-          </div>
-          <div className="nav-bar">
-            <nav>
-              <ul className="flex flex-row gap-10">
-                <li>About</li>
-                <li>Sign Up</li>
-                <li>Log In</li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <div className="main">
-
-        </div>
+    <div>
+      <div>
+        Top Part        
+      </div>
+      <div>
+        Main Part
       </div>
     </div>
   )
