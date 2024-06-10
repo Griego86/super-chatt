636c0af97b84508558b4e23a7b9a3ecf9f1f2a7c
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-09 17:35:40 -0700

    Refactor sign up page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,8 +2,6 @@ import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import DOMAIN from "../services/endpoint";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 
 export default function SignupPage() {
 
@@ -19,7 +17,7 @@ export default function SignupPage() {
         const res = await axios.post(`${DOMAIN}/api/users/`, newUser);
         if (res?.data.success) {
             setMessage(res?.data.message);
-            navigate("/capychat/users/login");
+            navigate("/superchatt/users/login");
         }
         else {
             setMessage(res?.data.message);
@@ -28,7 +26,6 @@ export default function SignupPage() {
 
     return (
         <div className="flex flex-col min-h-screen">
-            <Header />
             <main className="flex-1 mx-auto">
                 <form onSubmit={handleSubmit} className="flex flex-col">
                     <h2 className="py-10 text-2xl text-indigo-600 font-medium text-center">Sign Up</h2>
@@ -45,11 +42,10 @@ export default function SignupPage() {
                         <input type="email" name="email" id="email" placeholder="example@example.com" required className="px-2 border rounded-lg border-slate-700 py-1" />
                     </div>
                     <button className="rounded-xl my-5 py-2 px-2 bg-indigo-600 text-white">Sign Up</button>
-                    <NavLink to="/capychat/users/login" className="text-center text-indigo-600">Login</NavLink>
+                    <NavLink to="/superchatt/users/login" className="text-center text-indigo-600">Login</NavLink>
                 </form>
                 <p>{message}</p>
             </main>
-            <Footer />
         </div>
     )
 }
--- src/router.jsx
+++ src/router.jsx
@@ -2,13 +2,15 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import Layout from "./Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage';
 
 export function Router() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<Layout/>}>
         <Route path="/" element={<HomePage />}/>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/superchatt/users/login" element={<LoginPage />} />
+        <Route path="/superchatt/users/signup" element={<SignUpPage />} />
       </Route>
     )
   )
