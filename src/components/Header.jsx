c534dac0b2cc00fafb7b27cc7067b59b8b197e6f
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-14 14:41:51 -0700

    Merge branches

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,20 +13,20 @@ export default function Header() {
     return (
         <header className="sticky z-50 top-0 md:flex justify-between py-5 bg-indigo-600 text-white">
             {expandedMenu && <div className="flex flex-col md:flex-row">
-                <NavLink to="/capychat/" className="text-center py-2 md:py-4 px-5">Super Chatt</NavLink>
-                <NavLink to="/capychat/" className="flex py-2 md:py-4 mx-auto"><IoHomeSharp size={20} className="text-center mx-2" /> Home</NavLink>
+                <NavLink to="/superchatt/" className="text-center py-2 md:py-4 px-5">Super Chatt</NavLink>
+                <NavLink to="/superchatt/" className="flex py-2 md:py-4 mx-auto"><IoHomeSharp size={20} className="text-center mx-2" /> Home</NavLink>
             </div>}
             {expandedMenu && <div className="flex flex-col md:block md:py-4">
-                <NavLink to="/capychat/about" className="text-center py-2 md:py-4 px-5">About</NavLink>
-                <NavLink to="/capychat/contact" className="text-center py-2 md:py-4 px-5">Contact</NavLink>
+                <NavLink to="/superchatt/about" className="text-center py-2 md:py-4 px-5">About</NavLink>
+                <NavLink to="/superchatt/contact" className="text-center py-2 md:py-4 px-5">Contact</NavLink>
             </div>}
             {expandedMenu && <div className="flex flex-col md:block md:py-4">
-                <NavLink to="/capychat/users/login" className="text-center py-2 md:py-4 px-5">Login</NavLink>
-                <NavLink to="/capychat/users/signup" className="text-center py-2 md:py-4 px-5">Sign Up</NavLink>
+                <NavLink to="/superchatt/users/login" className="text-center py-2 md:py-4 px-5">Login</NavLink>
+                <NavLink to="/superchatt/users/signup" className="text-center py-2 md:py-4 px-5">Sign Up</NavLink>
             </div>}
             {expandedMenu && <div onClick={toggleMenu} className="text-center py-2 md:py-4 text-2xl md:hidden">&#127828;</div>}
             <div className="flex justify-between md:hidden">
-                {!expandedMenu && <NavLink to="/capychat/" className="py-2 px-2 md:hidden"><div className="flex">Super Chatt</div></NavLink>}
+                {!expandedMenu && <NavLink to="/superchatt/" className="py-2 px-2 md:hidden"><div className="flex">Super Chatt</div></NavLink>}
                 {!expandedMenu && <div onClick={toggleMenu} className="text-3xl md:hidden">&#127828;</div>}
             </div>
         </header>
